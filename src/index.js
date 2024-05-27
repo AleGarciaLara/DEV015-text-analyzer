3937016776272d013616948ba98057775d2ed116
Alejandra García <alejandra.garcialara@gmail.com>
2024-05-27 09:18:58 -0600

    Agregué funcionalidad a mi botón Limpiar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import analyzer from './analyzer.js';
 
+function resetText() {
+    const textarea = document.querySelector('textarea[name="user-input"]');
+    textarea.value = '';
+}
+document.getElementById('reset-button').addEventListener('click', resetText);
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
